dbb8909baf15045c6db6cc5909353c5578c2a7aa
wb_jinzhengjun <wb_jinzhengjun@meituan.com>
2023-06-26 17:48:32 +0800

    出差申请，查询个人待执行任务

--- acticity/src/test/java/com/jzj/test/ActivitiDemo.java
+++ acticity/src/test/java/com/jzj/test/ActivitiDemo.java
@@ -5,11 +5,14 @@ import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.List;
 
 @Slf4j
 public class ActivitiDemo {
@@ -17,7 +20,7 @@ public class ActivitiDemo {
      * 测试流程部署
      */
     @Test
-    public void testProcessDeployment(){
+    public void testProcessDeployment() {
         //1.创建 ProcessEnginess
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         //2.获取RepositoryService
@@ -25,8 +28,8 @@ public class ActivitiDemo {
         //3.使用service进行流程部署，定义一个流程名字，把bpmn文件和png部署到数据库中
         Deployment deploy = repositoryService.createDeployment()
                 .name("出差申请流程")
-                .addClasspathResource("bpmn"+ File.separator+"chuchai.bpmn")
-                .addClasspathResource("bpmn"+File.separator+"chuchai.png")
+                .addClasspathResource("bpmn" + File.separator + "chuchai.bpmn")
+                .addClasspathResource("bpmn" + File.separator + "chuchai.png")
                 .deploy();
         //4.输出整个部署信息
         log.info("流程部署id:{}", deploy.getId());
@@ -43,10 +46,9 @@ public class ActivitiDemo {
      * act_ru_execution 流程执行的信息
      * act_re_identityLink 流程参与者信息
      * act_ru_task 任务信息
-     *
      */
     @Test
-    public void startProcess(){
+    public void startProcess() {
         //1.创建processEngine（流程引擎）
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         //2.获取runtimeservice
@@ -57,7 +59,28 @@ public class ActivitiDemo {
         log.info("流程定义id:{}", processInstance.getProcessDefinitionId());
         log.info("流程实例id:{}", processInstance.getId());
         log.info("当前活动id:{}", processInstance.getActivityId());
+    }
 
+    /**
+     * 查询个人待执行的任务
+     */
+    @Test
+    public void findPersonTaskList() {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        TaskService taskService = processEngine.getTaskService();
+        //根据流程的key和任务的负责人来查询任务
+        List<Task> taskList = taskService.createTaskQuery()
+                .processDefinitionKey("chuchai")
+                .taskAssignee("zhangsan") //负责人
+                .list();
+
+        for (Task task : taskList) {
+            log.info("流程实例id：{}", task.getProcessInstanceId());
+            log.info("任务id：{}", task.getId());
+            log.info("任务负责人：{}", task.getAssignee());
+            log.info("任务名称：{}", task.getName());
+
+        }
 
     }
 }
